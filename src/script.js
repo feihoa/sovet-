aab76615e302aabe76d85b31a9c3b140d723e380
Maria Terenteva <rtillkriyt@gmail.com>
2021-09-29 13:48:26 +0300

    init

---
+++ src/script.js
@@ -0,0 +1,11 @@
+import M from 'materialize-css'
+import "./index.css";
+import "./images/REC.png";
+import "./images/TPP.png";
+import "./images/EEC.png";
+import "./images/CCPIT.png";
+import "./images/HYVE.png";
+import "./images/MF.png";
+import "./images/SLOVAK.jpg";
+import "./images/GAZ.jpg";
+
---
+++ webpack.config.js
@@ -0,0 +1,83 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackMd5Hash = require("webpack-md5-hash");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const webpack = require("webpack");
+const isDev = process.env.NODE_ENV === "development";
+
+module.exports = {
+  entry: {
+    main: "./src/script.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "./[name]/[name].[chunkhash].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        loader: "file-loader?name=./vendor/[name].[ext]",
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          isDev
+            ? "style-loader"
+            : {
+                loader: MiniCssExtractPlugin.loader,
+                options: {
+                  publicPath: "../",
+                },
+              },
+          "css-loader",
+          "postcss-loader",
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif|ico|svg)$/,
+        use: [
+          "file-loader?name=./images/[name].[ext]",
+          {
+            loader: "image-webpack-loader",
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "./[name]/[name].[contenthash].css",
+    }),
+
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require("cssnano"),
+      cssProcessorPluginOptions: {
+        preset: ["default"],
+      },
+      canPrint: true,
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: "./src/index.html",
+      filename: "index.html",
+      chunks: ["main"],
+      minify: {
+        collapseWhitespace: true,
+      },
+    }),
+    new webpack.DefinePlugin({
+      NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+    }),
+    new WebpackMd5Hash(),
+  ],
+};
