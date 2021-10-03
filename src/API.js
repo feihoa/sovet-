c8f6cd7d42f510b38b8369b905460101d3daf181
Maria Terenteva <rtillkriyt@gmail.com>
2021-10-03 23:49:15 +0300

    fixes

--- src/API.js
+++ src/API.js
@@ -5,7 +5,7 @@ export class Api {
       this.tel = tel;
       this.email = email;
       this.text = text;
-    return  fetch('http://104.198.106.89:3000/form', {
+    return  fetch('http://api.sovet-consult.tk/form', {
           method: 'POST',
           credentials: "include",
           body: JSON.stringify({
